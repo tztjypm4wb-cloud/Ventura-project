22153dce75f3d13d06c9186d1ab594700e02bdaa
tztjypm4wb-cloud <tztjypm4wb@privaterelay.appleid.com>
2026-08-06 08:26:25 +0300

    Update constants.dart

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hiddify/utils/utils.dart';
+import 'package:venturavpn/utils/utils.dart';
 
 abstract class Constants {
-  static const appName = "Hiddify";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
+  static const appName = "VenturaVPN";
+  static const githubUrl = "";
   static const licenseUrl = "https://github.com/hiddify/hiddify-next?tab=License-1-ov-file#readme";
-  static const githubReleasesApiUrl = "https://api.github.com/repos/hiddify/hiddify-next/releases";
-  static const githubLatestReleaseUrl = "https://github.com/hiddify/hiddify-app/releases/latest";
-  static const appCastUrl = "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/hiddify";
+  static const githubReleasesApiUrl = "";
+  static const githubLatestReleaseUrl = "";
+  static const appCastUrl = "";
+  static const telegramChannelUrl = "";
   static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
   static const termsAndConditionsUrl = "https://hiddify.com/terms/";
   static const cfWarpPrivacyPolicy = "https://www.cloudflare.com/application/privacypolicy/";
