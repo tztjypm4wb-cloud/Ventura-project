99938823b3a709d67408d54e93ade60676c54e24
tztjypm4wb-cloud <tztjypm4wb@privaterelay.appleid.com>
2026-08-06 08:30:55 +0300

    Update preferences_provider.dart

--- lib/core/preferences/preferences_provider.dart
+++ lib/core/preferences/preferences_provider.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:hiddify/core/model/environment.dart';
-import 'package:hiddify/utils/platform_utils.dart';
+import 'package:venturavpn/core/model/environment.dart';
+import 'package:venturavpn/utils/platform_utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loggy/loggy.dart';
 import 'package:path/path.dart' as p;
