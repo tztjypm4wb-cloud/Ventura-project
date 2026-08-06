daebb8dfc859f4479a46a0a4a44ca1566bdcd290
tztjypm4wb-cloud <tztjypm4wb@privaterelay.appleid.com>
2026-08-06 08:12:35 +0300

    Update analytics_controller.dart

--- lib/core/analytics/analytics_controller.dart
+++ lib/core/analytics/analytics_controller.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
-import 'package:hiddify/core/analytics/analytics_filter.dart';
-import 'package:hiddify/core/analytics/analytics_logger.dart';
+import 'package:venturavpn/core/analytics/analytics_filter.dart';
+import 'package:venturavpn/core/analytics/analytics_logger.dart';
 
-import 'package:hiddify/core/logger/logger_controller.dart';
-import 'package:hiddify/core/model/environment.dart';
-import 'package:hiddify/core/preferences/preferences_provider.dart';
-import 'package:hiddify/utils/custom_loggers.dart';
+import 'package:venturavpn/core/logger/logger_controller.dart';
+import 'package:venturavpn/core/model/environment.dart';
+import 'package:venturavpn/core/preferences/preferences_provider.dart';
+import 'package:venturavpn/utils/custom_loggers.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
